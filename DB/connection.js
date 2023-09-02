d2b50ec96d59fc70d1bda868fff799cdcc4ddf93
yousef <yousefmahmoudmahmed@outlook.com>
2023-09-03 00:28:59 +0300

    Fix Product.js

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose'
 const connectDB  = async ()=>{
-    console.log(process.env.DB_LOCAL);
-    return await mongoose.connect(process.env.DB_LOCAL)
+
+    return await mongoose.connect(process.env.DB_ATLAS,{
+        serverSelectionTimeoutMS:1000
+    })
     .then(res=>console.log(`DB Connected successfully on .........`))
     .catch(err=>console.log(` Fail to connect  DB.........${err} `))
 }
