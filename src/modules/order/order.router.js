d5871853c5e986f02c8d8bdb729773d802203a40
yousef <yousefmahmoudmahmed@outlook.com>
2023-09-22 23:12:19 +0300

    add validation

--- src/modules/order/order.router.js
+++ src/modules/order/order.router.js
@@ -4,14 +4,21 @@ import * as validator from "./order.validation.js";
 import orderEndpoint from "./order.endPoint.js";
 import { asyncHandler } from "../../utils/errorHandling.js";
 import { auth } from "../../middleware/auth.js";
+import { IdValidator, validation } from "../../middleware/validation.js";
 
 const router = Router();
 
 router.get(
   "/",
   auth(orderEndpoint.CURDoperation),
+  validation(validator.create),
   asyncHandler(orderController.createOrder)
 );
 router.post("/webhook", asyncHandler(orderController.webhook));
-router.put("/cancel-order/:id",  auth(orderEndpoint.CURDoperation),asyncHandler(orderController.cancelOrder));
+router.put(
+  "/cancel-order/:id",
+  auth(orderEndpoint.CURDoperation),
+  validation(IdValidator),
+  asyncHandler(orderController.cancelOrder)
+);
 export default router;
