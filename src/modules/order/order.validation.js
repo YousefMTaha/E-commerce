b6db53ffe85f404cc027a307da2cf2503f9550b2
yousef <yousefmahmoudmahmed@outlook.com>
2023-09-22 22:58:18 +0300

    add create order validation

--- src/modules/order/order.validation.js
+++ src/modules/order/order.validation.js
@@ -1,10 +1,31 @@
 import Joi from "joi";
 import { generalFields } from "../../middleware/validation.js";
 
-export const add = {
-    body:Joi.object({
-    address:generalFields.name.required(),
-    phone:generalFields.phone.required(),
-    paymentMethod:Joi.string().valid('cash','visa').required(),
-    }).required()
-}
+const productValidation = (value, helper) => {
+  const schema = Joi.object({
+    value: Joi.array().items({
+      productId: generalFields.id.required(),
+      quantity: Joi.number().min(0).required(),
+    }),
+  });
+  const result = schema.valid({ value });
+  return result.error ? helper.message(result.error) : true;
+};
+export const create = {
+  body: Joi.object({
+    address: generalFields.name.required(),
+    phone: generalFields.phone.required(),
+    coupon: generalFields.name.required(),
+    products: Joi.custom(productValidation).required(),
+    paymentMethod: Joi.string().valid("cash", "card"),
+    status: Joi.string().valid(
+      "placed",
+      "delivered",
+      "waitingPayment",
+      "canceled"
+    ),
+    notes: Joi.string(),
+  }).required(),
+  params: Joi.object({}).required(),
+  query: Joi.object({}).required(),
+};
